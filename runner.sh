d7e697c009d5e96421074b6337102ee737b213bb
Daniel Gander <dannygonline@gmail.com>
2021-11-09 19:19:08 +0000

    which node

--- runner.sh
+++ runner.sh
@@ -3,9 +3,11 @@
 echo "---- root folder ----"
 echo "node version from volta: $(volta run node --version)"
 echo "node version: $(node --version)"
+echo "which node: $(which node)"
 
 echo "---- sub folder ----"
 cd sub_folder
 echo "node version from volta: $(volta run node --version)"
 echo "node version: $(node --version)"
+echo "which node: $(which node)"
 
