41f1dcb7fef8ca7e128eb893469db3cde0039150
Daniel Gander <dannygonline@gmail.com>
2021-11-10 07:40:25 +0000

    messaging

--- runner.sh
+++ runner.sh
@@ -11,8 +11,8 @@ echo "node version from volta: $(volta run node --version)"
 echo "node version: $(node --version)"
 echo "which node: $(which node)"
 echo
-echo "sub runner run by with WITH volta --node"
+echo "sub runner run by yarn WITH volta --node"
 volta run yarn sub_runner --node "$(volta run node --version)"
-echo "sub runner run by with without volta --node"
+echo "sub runner run by yarn without volta"
 yarn sub_runner
 
